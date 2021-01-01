f4eb1c9ffd471a12c29bf79cde153b6fa2bfc068
martin211 <mail4evgeniy@gmail.com>
2021-01-01 17:53:52 +0300

    Build

--- build/Build.cs
+++ build/Build.cs
@@ -1,19 +1,13 @@
-using System;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
@@ -82,5 +76,17 @@ class Build : NukeBuild
                 .SetOutputDirectory(PackageDirectory)
                 .SetProperty("targetDir", $"{targetDir}\\")
                 .SetProperty("id", "GitExtensions.GitLabCommitHintPlugin"));
+        });
+
+    Target Deploy => _ => _
+        .Executes(() =>
+        {
+            var files = SourceDirectory.GlobDirectories(OutputDirectory / "packages").First().GlobFiles("*.nupkg");
+            foreach (var file in files)
+            {
+                DotNetNuGetPush(_ => _
+                    .SetTargetPath(file)
+                    .SetSource("GitLab"));
+            }
         });
 }
