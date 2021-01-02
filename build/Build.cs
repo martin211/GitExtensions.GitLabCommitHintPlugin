12047e2537faf13edec4b85665ad50ce5742af17
martin211 <mail4evgeniy@gmail.com>
2021-01-02 12:13:53 +0300

    Build

--- build/Build.cs
+++ build/Build.cs
@@ -58,14 +58,6 @@ class Build : NukeBuild
                 .SetAssemblyVersion(GitVersion.AssemblySemVer)
                 .SetFileVersion(GitVersion.AssemblySemFileVer)
                 .SetInformationalVersion(GitVersion.InformationalVersion));
-
-            //DotNetBuild(_ => _
-            //    .SetProjectFile(Solution)
-            //    .SetConfiguration(Configuration)
-            //    .SetAssemblyVersion(GitVersion.AssemblySemVer)
-            //    .SetFileVersion(GitVersion.AssemblySemFileVer)
-            //    .SetInformationalVersion(GitVersion.InformationalVersion)
-            //    .EnableNoRestore());
         });
 
     AbsolutePath PackageDirectory => OutputDirectory / "packages";
